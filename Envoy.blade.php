7281e98ece895502d7d12f59dc3ccbd000128294
carsonlius <carsonlius@163.com>
2019-01-07 20:44:58 +0800

    modify: envoy update test

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,19 @@
-@servers(['web1' => ['carsonlius@127.0.0.1'], 'web2' => ['carsonlius@172.31.154.122']])
+@servers(['web1' => '127.0.0.1', 'web2' => ['carsonlius@172.31.154.122']])
 
-@task('show', ['on' => [ 'web2'], 'parallel' => true])
+@task('pull_web2', ['on' => [ 'web2'], 'parallel' => true])
 cd laravel_learn
-ls -la
+git pull origin master
 @endtask
+
+@task('push_web1',  ['on' => [ 'web1'], 'parallel' => true])
+cd laravel.learn
+git add .
+git commit -m 'modify: envoy update test'
+git push
+echo 'well done!'
+@endtask
+
+@story('deploy')
+push_web1
+pull_web2
+@endstory
