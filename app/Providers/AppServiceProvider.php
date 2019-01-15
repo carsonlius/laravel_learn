67a9a1a69d213b43a97915682c57a810e3a0dcd7
carsonlius <carsonlius@163.com>
2019-01-15 10:31:46 +0800

    modify: queue failing event

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Queue::failing(function(JobFailed $event){
+
+
+        });
     }
 
     /**
